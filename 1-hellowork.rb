53bbeea2f7846d311ec5458e86f7d459e3298154
marckm <maxime.marck@live.fr>
2014-05-02 20:18:16 +0200

    removed 3 files that were moved in another folder

--- 1-hellowork.rb
+++
@@ -1,9 +0,0 @@
-class Hello
-	def say_hello()
-		puts "Hello World from Ruby"
-	end
-end
-
-
-hello = Hello.new
-hello.say_hello
--- 3-ruby_strings.rb
+++
@@ -1,6 +0,0 @@
-puts 'I am not multiline\nsentence'
-puts "I am multiline\n sentence"
-
-name = "Scott Tiger"
-puts "Hello #{name} how are you today?"
-puts "Hello " + name + " how are you today?"
